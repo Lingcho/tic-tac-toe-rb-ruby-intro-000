ce2af9183b6c881fbb1d65fc8cdf157de5e89ac6
Nguyen Van Say <vansay@hotmail.fr>
2020-03-11 14:25:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,7 +35,7 @@ def turn(board)
   if valid_move?(board, index)
     move(board, index, current_player(board))
     display_board(board)
-    
+
   else
     turn(board)
   end
@@ -111,5 +111,3 @@ end
 def play(board)
 
 end
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-p turn(board)

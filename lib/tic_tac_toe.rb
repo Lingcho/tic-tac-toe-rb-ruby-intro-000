a48482158eb7bd740fb65f611dd4f7b16cfec48a
Nguyen Van Say <vansay@hotmail.fr>
2020-03-11 14:42:39 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -25,7 +25,7 @@ def position_taken?(board, location)
 end
 
 def valid_move?(board, index)
-  index.between?(0,9) && !position_taken?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
 end
 
 def turn(board)

9cb5526b81f968ffc4f64f61294444219ef6f704
Nguyen Van Say <vansay@hotmail.fr>
2020-03-11 14:10:10 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,110 @@
+WIN_COMBINATIONS = [
+  [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]
+]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, player)
+  board[index] = player
+end
+
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
+def valid_move?(board, index)
+  index.between?(0,9) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index, current_player)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  counter = 0
+  board.each do |element|
+    if element == "X" || element == "O" then counter += 1 end
+  end
+  counter
+end
+
+def current_player(board)
+  if turn_count(board).even?
+    "X"
+  elsif turn_count(board).odd?
+    "O"
+  end
+end
+
+def won?(board)
+
+WIN_COMBINATIONS.each do |win_combination|
+
+  position_1 = board[win_combination[0]]
+  position_2 = board[win_combination[1]]
+  position_3 = board[win_combination[2]]
+
+  if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
+    return win_combination
+  else
+    false
+  end
+  end
+  if full?(board) == false
+    return false
+  end
+end
+
+def full?(board)
+  board.all? do |spot|
+    spot == "X" || spot == "O"
+  end
+end
+
+def draw?(board)
+
+  if won?(board)
+    false
+  elsif full?(board)
+    true
+  else
+    false
+  end
+end
+
+def over?(board)
+  if won?(board) || draw?(board) || full?(board)
+    true
+  else
+    false
+  end
+end
+
+def winner(board)
+  if won?(board)
+  row_win = won?(board)
+  board[row_win[0]]
+end
+end
+
+def play(board)
+  
+end
